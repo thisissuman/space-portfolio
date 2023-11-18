1635400d609e4fc3e9a688bca7f577acec9a925e
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-11-19 00:12:12 +0530

    Navbar

---
+++ components/main/Skills.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Skills = () => {
+  return (
+    <section
+      className="flex flex-col items-center  justify-center gap-3 h-full relative overflow-hidden py-20"
+      style={{ transform: "scale(0.9)" }}
+    ></section>
+  );
+};
+
+export default Skills;
