8caf97e0cf2f65e623897a57749944a3d8292860
thisissuman <43521912+thisissuman@users.noreply.github.com>
2023-11-20 21:45:11 +0530

    skill section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Hero from '@/components/main/Hero'
+import Skills from '@/components/main/Skills'
+import SkillsDataProvider from '@/components/sub/SkillsDataProvider'
 import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className="h-full w-full">
-      <div className="flex flex-col h-[850px] gap-20">
+      <div className="flex flex-col  gap-20">
         <Hero/>
+        <Skills/>
       </div>
     </main>
   )
